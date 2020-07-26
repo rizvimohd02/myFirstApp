753df5f8533a831d370b78b28dd834f5c15b447f
rizvimohd02 <68018392+rizvimohd02@users.noreply.github.com>
2020-07-26 11:37:50 +0530

    Add files via upload

---
+++ 26-Jul/business-add.js
@@ -0,0 +1,284 @@
+import React from 'react';
+import { StyleSheet, View, Text, TextInput, TouchableOpacity, Alert } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import PickerSelect from 'react-native-picker-select';
+import { CheckedIcon, UncheckedIcon } from '../images/svg-icons';
+import Geolocation from '@react-native-community/geolocation';
+
+import {addB, userID } from '../lib/utils'
+
+const styles = StyleSheet.create({
+  outerView: {
+    flex: 1,
+    padding: 15,
+    backgroundColor: '#FFF'
+  },
+  splitView: {
+    flexDirection: 'row',
+    borderColor: '#1e90ff',
+    borderWidth: 2,
+    borderRadius: 20,
+    padding: 8,
+    paddingBottom: 0,
+    justifyContent: 'space-between'
+    
+  },
+  timingArea: {
+    width: '50%',
+  },
+  label: {
+    fontFamily: 'IBMPlexSans-Medium',
+    color: '#000',
+    fontSize: 14,
+    paddingBottom: 5
+  },
+  selector: {
+    fontFamily: 'IBMPlexSans-Medium',
+    borderColor: '#D0E2FF',
+    borderWidth: 2,
+    padding: 8,
+    marginBottom: 15
+  },
+
+  textInput: {
+    fontFamily: 'IBMPlexSans-Medium',
+    flex: 1,
+    borderColor: '#1e90ff',
+    borderWidth: 2,
+    borderRadius: 20,
+    padding: 8,
+    elevation: 0,
+    marginBottom: 15
+  },
+  textCenterAlign: {
+    fontFamily: 'IBMPlexSans-Medium',
+    flex: 1,
+    borderColor: '#1e90ff',
+    borderWidth: 2,
+    borderRadius: 20,
+    padding: 8,
+    elevation: 0,
+    textAlign:'center',
+    marginBottom: 15
+  },
+  checkboxContainer: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    marginBottom: 10
+  },
+  checkboxLabel: {
+    fontFamily: 'IBMPlexSans-Light',
+    fontSize: 13
+  },
+  textInputDisabled: {
+    fontFamily: 'IBMPlexSans-Medium',
+    backgroundColor: '#f4f4f4',
+    color: '#999',
+    flex: 1,
+    borderColor: '#1e90ff',
+    borderWidth: 2,
+    borderRadius: 20,
+    padding: 8,
+    elevation: 0,
+    marginBottom: 15
+  },
+  button: {
+    backgroundColor: '#1e90ff',
+    color: '#FFFFFF',
+    fontFamily: 'IBMPlexSans-Medium',
+    fontSize: 16,
+    overflow: 'hidden',
+    borderRadius: 20,
+    padding: 14,
+    textAlign:'center',
+    marginTop: 10,
+    marginBottom: 35
+  }
+});
+
+const AddBusiness = function ({navigation }) {  
+  const clearItem = { userID: userID(),businessname: '', openingtime: 'Select Time', closingtime: 'Select Time', personallowed: '', isBookingMand: '', location: '', trnsctype: 'BusinessRegistration' }
+  const [item, setItem] = React.useState(clearItem);
+  const [useLocation, setUseLocation] = React.useState(true);
+  const [position, setPosition] = React.useState({})
+  
+  React.useEffect(() => {
+    navigation.addListener('focus', () => {
+      Geolocation.getCurrentPosition((pos) => {
+        setPosition(pos)
+        if (useLocation) {
+          setItem({
+            ...item,
+            location: `${pos.coords.latitude},${pos.coords.longitude}`
+          })
+        }
+      });
+    })
+  }, []);
+
+  const toggleUseLocation = () => {
+    if (!useLocation && position) {
+      setItem({
+        ...item,
+        location: `${position.coords.latitude},${position.coords.longitude}`
+      })
+    }
+    setUseLocation(!useLocation);
+  };
+
+    const sendItem = () => {
+    const payload = {
+      ...item,
+      personallowed: isNaN(item.personallowed) ? 1 : parseInt(item.personallowed)
+    };
+
+    addB(payload)
+      .then(() => {
+        Alert.alert('Thank you!', 'Your Business details has been added.', [{text: 'OK'}]);
+        setItem({ ...clearItem, location: payload.location });
+      })
+      .catch(err => {
+        console.log(err);
+        Alert.alert('ERROR', 'Please try again. If the problem persists contact an administrator.', [{text: 'OK'}]);
+      });
+  };
+
+  return (
+    <ScrollView style={styles.outerView}>
+      <Text style={styles.label}>Business Name</Text>
+      <TextInput
+        style={styles.textInput}
+        value={item.businessname}
+        onChangeText={(t) => setItem({...item, businessname: t})}
+        onSubmitEditing={sendItem}
+        returnKeyType='send'
+        enablesReturnKeyAutomatically={true}
+        placeholder='e.g. Reliance Fresh'
+        blurOnSubmit={false}
+      />
+
+
+     <Text style={styles.label}>No. of Persons Allowed / slot</Text>
+      <TextInput
+        style={styles.textInput}
+        value={item.personallowed}
+        onChangeText={(t) => setItem({...item, personallowed: t})}
+        onSubmitEditing={sendItem}
+        returnKeyType='send'
+        enablesReturnKeyAutomatically={true}
+        placeholder='e.g., 25'
+      />
+
+<Text style={styles.label}>Business Operating Hours</Text>
+<View style={styles.splitView}>
+        <View style={styles.timingArea}>
+          <Text style={styles.label}>Opening Time</Text>
+          <PickerSelect
+            style={{inputIOS: styles.selector }}
+            value={item.openingtime}
+            onValueChange={(t) => setItem({...item, openingtime: t })}
+            items={[
+              
+                {label: 'Select Time', value: 'Select Time' },
+                {label: '7 AM', value: '7 AM' },
+                {label: '8 AM', value: '8 AM' },
+                {label: '9 AM', value: '9 AM' },
+                {label: '10 AM', value: '10 AM' },
+                {label: '11 AM', value: '11 AM' },
+                {label: '12 PM', value: '12 PM' },
+                {label: '1 PM', value: '1 PM' },
+                {label: '2 PM', value: '2 PM' },
+                {label: '3 PM', value: '3 PM' },
+                {label: '4 PM', value: '4 PM' },
+                {label: '5 PM', value: '5 PM' },
+                {label: '6 PM', value: '6 PM' },
+                {label: '7 PM', value: '7 PM' },
+                {label: '8 PM', value: '8 PM' },
+                {label: '9 PM', value: '9 PM' },
+                {label: '10 PM', value: '10 PM' },
+                {label: '11 PM', value: '11 PM' },
+            ]}
+          />
+        </View>
+        <View style={styles.timingArea}>
+          <Text style={styles.label}>Closing Time</Text>
+          <PickerSelect
+            style={{inputIOS: styles.selector }}
+            value={item.closingtime}
+            onValueChange={(t) => setItem({...item, closingtime: t })}
+            items={[
+              
+                {label: 'Select Time', value: 'Select Time' },
+                {label: '7 AM', value: '7 AM' },
+                {label: '8 AM', value: '8 AM' },
+                {label: '9 AM', value: '9 AM' },
+                {label: '10 AM', value: '10 AM' },
+                {label: '11 AM', value: '11 AM' },
+                {label: '12 PM', value: '12 PM' },
+                {label: '1 PM', value: '1 PM' },
+                {label: '2 PM', value: '2 PM' },
+                {label: '3 PM', value: '3 PM' },
+                {label: '4 PM', value: '4 PM' },
+                {label: '5 PM', value: '5 PM' },
+                {label: '6 PM', value: '6 PM' },
+                {label: '7 PM', value: '7 PM' },
+                {label: '8 PM', value: '8 PM' },
+                {label: '9 PM', value: '9 PM' },
+                {label: '10 PM', value: '10 PM' },
+                {label: '11 PM', value: '11 PM' },
+            ]}
+          />
+        </View>
+      </View>    
+
+      <Text style={styles.label}> </Text>
+      <Text style={styles.label}>Is Booking Mandatory?</Text>
+      <TextInput
+        style={styles.textInput}
+        value={item.isBookingMand}
+        onChangeText={(t) => setItem({...item, isBookingMand: t})}
+        onSubmitEditing={sendItem}
+        returnKeyType='send'
+        enablesReturnKeyAutomatically={true}
+        placeholder='Yes or No'
+      />
+
+      <Text style={styles.label}>Location</Text>
+      <View style={styles.checkboxContainer}>
+        <TouchableOpacity onPress={toggleUseLocation}>
+          {
+            (useLocation)
+              ?
+              <CheckedIcon height='18' width='18'/>
+              :
+              <UncheckedIcon height='18' width='18'/>
+          }
+        </TouchableOpacity>
+        <Text style={styles.checkboxLabel}> Use my current location </Text>
+      </View>
+      <TextInput
+        style={useLocation ? styles.textInputDisabled : styles.textInput}
+        value={item.location}
+        onChangeText={(t) => setItem({ ...item, location: t})}
+        onSubmitEditing={sendItem}
+        returnKeyType='send'
+        enablesReturnKeyAutomatically={true}
+        placeholder='street address, city, state'
+        editable={!useLocation}
+      />
+      
+      {
+        item.businessname !== '' &&
+        item.openingtime.trim() !== '' &&
+        item.closingtime.trim() !== '' &&
+        item.personallowed !== '' &&
+        
+        <TouchableOpacity onPress={sendItem}>
+          <Text style={styles.button}>Submit</Text>
+        </TouchableOpacity>
+      }
+    </ScrollView>
+  );
+};
+
+export default AddBusiness;
---
+++ 26-Jul/server.js
@@ -0,0 +1,333 @@
+require('dotenv').config({silent: true})
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const assistant = require('./lib/assistant.js');
+const port = process.env.PORT || 3000
+
+const cloudant = require('./lib/cloudant.js');
+
+const app = express();
+app.use(bodyParser.json());
+
+const testConnections = () => {
+  const status = {}
+  return assistant.session()
+    .then(sessionid => {
+      status['assistant'] = 'ok';
+      return status
+    })
+    .catch(err => {
+      console.error(err);
+      status['assistant'] = 'failed';
+      return status
+    })
+    .then(status => {
+      return cloudant.info();
+    })
+    .then(info => {
+      status['cloudant'] = 'ok';
+      return status
+    })
+    .catch(err => {
+      console.error(err);
+      status['cloudant'] = 'failed';
+      return status
+    });
+};
+
+const handleError = (res, err) => {
+  const status = err.code !== undefined && err.code > 0 ? err.code : 500;
+  return res.status(status).json(err);
+};
+
+app.get('/', (req, res) => {
+  testConnections().then(status => res.json({ status: status }));
+});
+
+/**
+ * Get a session ID
+ *
+ * Returns a session ID that can be used in subsequent message API calls.
+ */
+app.get('/api/session', (req, res) => {
+  assistant
+    .session()
+    .then(sessionid => res.send(sessionid))
+    .catch(err => handleError(res, err));
+});
+
+/**
+ * Post process the response from Watson Assistant
+ *
+ * We want to see if this was a request for resources/supplies, and if so
+ * look up in the Cloudant DB whether any of the requested resources are
+ * available. If so, we insert a list of the resouces found into the response
+ * that will sent back to the client.
+ * 
+ * We also modify the text response to match the above.
+ */
+function post_process_assistant(result) {
+  let resource
+  // First we look to see if a) Watson did identify an intent (as opposed to not
+  // understanding it at all), and if it did, then b) see if it matched a supplies entity
+  // with reasonable confidence. "supplies" is the term our trained Watson skill uses
+  // to identify the target of a question about resources, i.e.:
+  //
+  // "Where can i find face-masks?"
+  //
+  // ....should return with an enitity == "supplies" and entitty.value = "face-masks"
+  //
+  // That's our trigger to do a lookup - using the entitty.value as the name of resource
+  // to to a datbase lookup.
+  if (result.intents.length > 0 ) {
+    result.entities.forEach(item => {
+      if ((item.entity == "supplies") &&  (item.confidence > 0.3)) {
+        resource = item.value
+      }
+    })
+  }
+  if (!resource) {
+    return Promise.resolve(result)
+  } else {
+    // OK, we have a resource...let's look this up in the DB and see if anyone has any.
+    return cloudant
+      .find('', resource, '')
+      .then(data => {
+        let processed_result = result
+        if ((data.statusCode == 200) && (data.data != "[]")) {
+          processed_result["resources"] = JSON.parse(data.data)
+          processed_result["generic"][0]["text"] = 'There is' + '\xa0' + resource + " available"
+        } else {
+          processed_result["generic"][0]["text"] = "Sorry, no" + '\xa0' + resource + " available"           
+        }
+        return processed_result
+      })
+  }
+}
+
+/**
+ * Post a messge to Watson Assistant
+ *
+ * The body must contain:
+ * 
+ * - Message text
+ * - sessionID (previsoulsy obtained by called /api/session)
+ */
+app.post('/api/message', (req, res) => {
+  const text = req.body.text || '';
+  const sessionid = req.body.sessionid;
+  console.log(req.body)
+  assistant
+    .message(text, sessionid)
+    .then(result => {
+      return post_process_assistant(result)
+    })
+    .then(new_result => {
+      res.json(new_result)
+    })
+    .catch(err => handleError(res, err));
+});
+
+/**
+ * Get a list of resources
+ *
+ * The query string may contain the following qualifiers:
+ * 
+ * - place
+ * - name
+ * - userID
+ *
+ * A list of resource objects will be returned (which can be an empty list)
+ */
+app.get('/api/resource', (req, res) => {
+
+  if (req.body.trnsctype == 'customerBooking'){
+    const place = req.query.place;
+    const name = req.query.name;
+    const userID = req.query.userID;
+    const trnsctype = req.query.trnsctype;
+    cloudant
+      .find(place, name, userID, trnsctype)
+      .then(data => {
+        if (data.statusCode != 200) {
+          res.sendStatus(data.statusCode)
+        } else {
+          res.send(data.data)
+        }
+      })
+      .catch(err => handleError(res, err));
+
+  } elseif (req.body.trnsctype == 'StaffDetails'); {
+
+    const staffName = req.query.staffName;
+    const userID = req.query.userID;
+    const trnsctype = req.query.trnsctype;
+    cloudant
+      .findStaff(staffName, userID, trnsctype)
+      .then(data => {
+        if (data.statusCode != 200) {
+          res.sendStatus(data.statusCode)
+        } else {
+          res.send(data.data)
+        }
+      })
+      .catch(err => handleError(res, err));
+  }
+
+});
+
+/**
+ * Create a new resource
+ *
+ * The body must contain:
+ * 
+ * - place
+ * - name
+ * - contact
+ * - userID
+ * - datetime
+ * - bookingtime
+ * - trnsctype
+ * The body may also contain:
+ * 
+ * - emailid
+ * - person (which will default to 1 if not included)
+ * 
+ * The ID and rev of the resource will be returned if successful
+ */
+//let places = ["Food", "Other", "Help"]
+app.post('/api/resource', (req, res) => {
+
+  if (req.body.name){
+
+    if (!req.body.place) {
+      return res.status(422).json({ errors: "Type of item must be provided"});
+    }
+    if (!req.body.name) {
+      return res.status(422).json({ errors: "Name of item must be provided"});
+    }
+    if (!req.body.contact) {
+      return res.status(422).json({ errors: "A method of conact must be provided"});
+    }
+    if (!req.body.datetime) {
+      return res.status(422).json({ errors: "Date must be provided"});
+    }
+    if (!req.body.bookingtime) {
+      return res.status(422).json({ errors: "Timeslot must be provided"});
+    }
+    const place = req.body.place;
+    const name = req.body.name;
+    const emailid = req.body.emailid || '';
+    const userID = req.body.userID || '';
+    const person = req.body.person || 1;
+    const contact = req.body.contact;
+    const datetime = req.body.datetime;
+    const bookingtime = req.body.bookingtime;
+    const trnsctype = req.body.trnsctype;
+  
+    cloudant
+      .create(place, name, emailid, person, contact, userID, datetime, bookingtime, trnsctype)
+      .then(data => {
+        if (data.statusCode != 201) {
+          res.sendStatus(data.statusCode)
+        } else {
+          res.send(data.data)
+        }
+      })
+      .catch(err => handleError(res, err));
+
+  } else if (req.body.businessname) {
+  
+  const businessname = req.body.businessname;
+  const openingtime = req.body.openingtime;
+  const closingtime = req.body.closingtime || '';
+  const userID = req.body.userID || '';
+  const personallowed = req.body.personallowed || 1;
+  const isBookingMand = req.body.isBookingMand;
+  const location = req.body.location || '';
+  const trnsctype = req.body.trnsctype;
+
+  cloudant
+    .createB(businessname, openingtime, closingtime, personallowed, isBookingMand, location, trnsctype, userID)
+    .then(data => {
+      if (data.statusCode != 201) {
+        res.sendStatus(data.statusCode)
+      } else {
+        res.send(data.data)
+      }
+    })
+    .catch(err => handleError(res, err));
+
+  } else if (req.body.staffName){
+
+    const staffName = req.body.staffName;
+    const staffEmailid = req.body.staffEmailid;
+    const staffContact = req.body.staffContact;
+    const staffDepartment = req.body.staffDepartment || '';
+    const trnsctype = req.body.trnsctype;
+    const userID = req.body.userID || '';
+    
+    cloudant
+      .createStaff(staffName, staffEmailid, staffContact, staffDepartment, trnsctype, userID)
+      .then(data => {
+        if (data.statusCode != 201) {
+          res.sendStatus(data.statusCode)
+        } else {
+          res.send(data.data)
+        }
+      })
+      .catch(err => handleError(res, err));
+
+  }
+
+});
+
+/**
+ * Update new resource
+ *
+ * The body may contain any of the valid attributes, with their new values. Attributes
+ * not included will be left unmodified.
+ * 
+ * The new rev of the resource will be returned if successful
+ */
+
+app.patch('/api/resource/:id', (req, res) => {
+  const place = req.body.place || '';
+  const name = req.body.name || '';
+  const emailid = req.body.emailid || '';
+  const userID = req.body.userID || '';
+  const person = req.body.person || '';
+  const contact = req.body.contact || '';
+  const datetime = req.body.datetime || '';
+  const bookingtime = req.body.bookingtime || '';
+  const trnsctype = req.body.trnsctype || '';
+
+  cloudant
+    .update(req.params.id, place, name, emailid, person, contact, userID, datetime, bookingtime, trnsctype)
+    .then(data => {
+      if (data.statusCode != 200) {
+        res.sendStatus(data.statusCode)
+      } else {
+        res.send(data.data)
+      }
+    })
+    .catch(err => handleError(res, err));
+});
+
+/**
+ * Delete a resource
+ */
+app.delete('/api/resource/:id', (req, res) => {
+  cloudant
+    .deleteById(req.params.id)
+    .then(statusCode => res.sendStatus(statusCode))
+    .catch(err => handleError(res, err));
+});
+
+const server = app.listen(port, () => {
+   const host = server.address().address;
+   const port = server.address().port;
+   console.log(`SolutionStarterKitCooperationServer listening at http://${host}:${port}`);
+});
